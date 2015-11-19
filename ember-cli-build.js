deaaaaabad83a0a3d2edad099c2d178a500bfa68
Lou Alicegary <lou@ginkgotree.com>
2015-11-19 14:31:01 -0500

    making player table a component and prepping for bootstrap table

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,15 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+  
   var app = new EmberApp(defaults, {
+    
     // Add options here
+    'ember-bootstrap': {
+        'importBootstrapJS': true,
+        'importBootstrapTheme': true
+    }
+  
   });
 
   // Use `app.import` to add additional libraries to the generated
---
+++ tests/integration/components/player-table-test.js
@@ -0,0 +1,26 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('player-table', 'Integration | Component | player table', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+
+  this.render(hbs`{{player-table}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:" + EOL +
+  this.render(hbs`
+    {{#player-table}}
+      template block text
+    {{/player-table}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+
+});
